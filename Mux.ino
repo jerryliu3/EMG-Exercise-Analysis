6f9e473a78572d50fcbaf8bbbdbab7d55825f7e0
Jerry Liu <3jerryliu@gmail.com>
2018-02-26 22:32:03 -0500

    Add final mux code (partially working)

--- Mux.ino
+++ Mux.ino
@@ -4,8 +4,8 @@ int selectorThree = 10;
 int selectorFour = 11;
 boolean choose = true;
 int i = 0;
-double firstArray[]= [0,0];
-
+float myArray[]= {0,0};
+int sensorValue = analogRead(A5);
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -19,35 +19,34 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  if(i == 2000){
-    i = 0;
-  }
-  else if(i<=1000) {
-    pinMode(selectorOne, INPUT);
-    pinMode(selectorTwo, INPUT);
-    pinMode(selectorThree, OUTPUT);
-    pinMode(selectorFour, OUTPUT);
-    digitalWrite(selectorThree, LOW);
-    digitalWrite(selectorFour, LOW);
-  }
-  else {
-    pinMode(selectorOne, OUTPUT);
-    pinMode(selectorTwo, OUTPUT);
-    digitalWrite(selectorOne, LOW);
-    digitalWrite(selectorTwo, LOW);
-    pinMode(selectorThree, INPUT);
-    pinMode(selectorFour, INPUT);
-  }
-
+  pinMode(selectorOne, INPUT);
+  pinMode(selectorTwo, INPUT);
+  pinMode(selectorThree, OUTPUT);
+  pinMode(selectorFour, OUTPUT);
+  digitalWrite(selectorThree, LOW);
+  digitalWrite(selectorFour, LOW);
+  //delay(300);
+    
+  // read the input on analog pin 5:
+  sensorValue = analogRead(A5);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 10V):
+  myArray[0] =  sensorValue * (10.0 / 1023.0);
+    
+  pinMode(selectorOne, OUTPUT);
+  pinMode(selectorTwo, OUTPUT);
+  digitalWrite(selectorOne, LOW);
+  digitalWrite(selectorTwo, LOW);
+  pinMode(selectorThree, INPUT);
+  pinMode(selectorFour, INPUT);
+  //delay(300);
 
   // read the input on analog pin 5:
-  int sensorValue = analogRead(A5);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (10.0 / 1023.0);
-  // print out the value you read:
-  Serial.print(voltage);
-  Serial.print("\t");
-  Serial.println(i);
+  sensorValue = analogRead(A5);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 10V):
+  myArray[1] = sensorValue * (10.0 / 1023.0);
   
-  i++;
+  // print out the value you read:
+  Serial.print(myArray[0]);
+  Serial.print(",");
+  Serial.println(myArray[1]);
 }
